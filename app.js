b05fd3541125ae6cc6da8eab4da0fd6d4902316c
neuralsearch-opensearch <neuralsearch.opensearch@gmail.com>
2023-04-18 13:20:23 +0300

    .

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require("express")
 app = express()
 
-app.get('/', (req,res)=>res.send("Hello World!"))
+app.get('/', (req,res)=>res.send("Hello World2! Secret:"+process.env.SECRET + '; Var: '+process.env.NAME_VAR))
 
 app.get('/mars', (req,res)=>res.send("Hello Mars!"))
 
