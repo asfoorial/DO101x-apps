ab8e4b639ca27f26418fe77f241329531674457a
neuralsearch-opensearch <neuralsearch.opensearch@gmail.com>
2023-04-18 12:42:16 +0300

    .

---
+++ version2/app.js
@@ -0,0 +1,8 @@
+var express = require("express")
+app = express()
+
+app.get('/', (req,res)=>res.send("Hello World3!"))
+
+app.get('/mars', (req,res)=>res.send("Hello Mars!"))
+
+app.listen(8080, ()=>console.log("App is listening on 8080"))
